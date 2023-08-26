4227c60258dfe4f5b4880fb323ae52f88d700f0a
pengu <bortiznine@gmail.com>
2023-08-26 10:20:47 -0500

    Added Assignment 1 for CS324

---
+++ A1_P6_Prime_Factorization_LASTNAME_FIRSTNAME.java
@@ -0,0 +1,102 @@
+//public class A1_P6_Prime_Factorization_LASTNAME_FIRSTNAME {
+//
+//   public static String name = "LASTNAME_FIRSTNAME";
+//
+//   public static void printPrimeFactorization(int n)  {
+//
+//
+//
+//
+//
+//   }
+//
+//
+//
+//
+////********************************************************************
+//// DO NOT CHANGE ANYTHING BELOW THIS LINE!!!
+////*********************************************************************
+//
+//   public static void main(String [] args)  {
+//
+//      sopln("\n" + name);
+//      sopln("A1_P6_Prime_Factoriation.\n");
+//
+//      printPrimeFactorization(720);
+//      sopln("\n");
+//
+//      for (int n = 2; n <= 20; n++) {
+//         printPrimeFactorization(n);
+//         sopln();
+//      }
+//
+//      sopln();
+//
+//      for (int n = 1000; n <= 1020; n++) {
+//         printPrimeFactorization(n);
+//         sopln();
+//      }
+//
+//   } // end main
+//
+//
+//
+//
+//
+//
+//   public static void sop(int a)
+//   {
+//      System.out.print(a);
+//   }
+//
+//   public static void sopln(int a)
+//   {
+//      System.out.println(a);
+//   }
+//
+//   public static void sop(boolean a)
+//   {
+//      System.out.print(a);
+//   }
+//
+//   public static void sopln(boolean a)
+//   {
+//      System.out.println(a);
+//   }
+//
+//
+//   public static void sop(double a)
+//   {
+//      System.out.print(a);
+//   }
+//
+//   public static void sopln(double a)
+//   {
+//      System.out.println(a);
+//   }
+//
+//   public static void sop(byte a)
+//   {
+//      System.out.print(a);
+//   }
+//
+//   public static void sopln(byte a)
+//   {
+//      System.out.println(a);
+//   }
+//
+//   public static void sop(String s)
+//   {
+//      System.out.print(s);
+//   }
+//   public static void sopln(String s)
+//   {
+//      System.out.println(s);
+//   }
+//   public static void sopln()
+//   {
+//      System.out.println();
+//   }
+//
+//
+//} // end class
